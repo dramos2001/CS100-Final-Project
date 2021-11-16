069e9a014e548d594281bed8f6507b6a9feff652
dramos2001 <damianramos00@gmail.com>
2021-11-15 16:58:30 -0800

    Added game class definition

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 
+// class definitions
+#include "game.hpp"
+
 // USING STATEMENTS
 using std::cout;
 using std::endl;
 using std::cin;
 
-// FUNCTION PROTOTYPES
-int gameStartUpScreen();
-
 
 // MAIN DRIVER CODE STARTS HERE
 int main() {
+    Game game;
     int choice = 0;
 
-    choice = gameStartUpScreen();
+    choice = game.start();
     if (choice == 2) {
         cout << "Thanks for playing, until next time...\n";
         exit(EXIT_SUCCESS);
@@ -30,20 +31,5 @@ int main() {
 
 
     return 0;
-}
-
-
-// gameStartUpScreen function definition
-// displays the startup screen of the game and asks the user whether or not they
-// want to play the game
-int gameStartUpScreen() {
-    int choice;
-    //cout << std::string(80, '*') << endl;
-    //cout << "*" << std::string(78, ' ') << "*" << endl;
-    cout << "WELCOME TO DRAGONSLAYER" << endl;
-    cout << "Press 1 to start your quest" << endl;
-    cout << "Press 2 to exit your quest" << endl << endl << endl;
-    cin >> choice;
-    return choice;
 }
 
