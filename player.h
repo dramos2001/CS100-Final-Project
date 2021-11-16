c0f1902cc6eb232d8dcacd8932de96a9afeca0c1
jasonguan067 <jasonguan067@gmail.com>
2021-11-15 18:59:34 -0800

    changed syntax and commented for Mob

--- player.h
+++ player.h
@@ -1,93 +1,64 @@
-#ifdef _PLAYER_HPP_
-#define _PLAYER_HPP_
+fndef __PLAYER_HPP__
+#define __PLAYER_HPP__
+#include <iostream>
+using namespace std; 
 #include <string>
-class Player {
 
-    private:
+class Player
+{
+private:
     string role;
-    string name;
-    int level;
+    string name; 
+    int level; 
     int exp;
-    int points;
-    int health;
-    int damage;
+    int points; 
+    int health; 
+    int damage; 
     int armor;
     int defense;
     int mana;
 
-    public:
-    Player(string name, string role){
+public: 
+    Player(string name, string role)
+    {
         this->name = name;
-        this->role = role;
+        this->role = role; 
+        level = 1; 
+        exp = 0;
+        points = 0;
+        health = 50; 
+        damage = 0; 
+        armor = 0; 
+        defense = 0; 
+        mana = 0; 
     }
 
-    Role* getRole(){
-        
-    }
+    virtual string getRole() const {return this->role; }
 
-    void setRole(string role){
-        this->role = role;
-    }
+    string getName() const {return this->name; }
+    int getLevel() const {return this->level; }
+    int getEXP() const {return this->exp; }
+    int getHealth() const {return this->health;}
+    int getMana() const {return this->mana; } 
+    int getPoints() const {return this->points; }
 
-    string getName(){
-        return name;
-    }
+    void setRole(string newRole) {this->role = newRole; }
+    void setName(string name) {this->name = name;}
+    void setLevel(int level) {this->level = level; }
+    void setEXP(int EXP) {this->exp = EXP; }
+    void setPoints(int points) {this->points = points; }
 
-    void setLevel(int level){
-        this->level = level;
-    }
+    void setHealth(int health) {this->health = health; }
+    void setMana(int mana) {this->mana = mana; }
+    void setArmor(int armor) {this->armor = armor; }
 
-    int getLevel(){
-        return level;
-    }
+    virtual void attack() { } 
+    virtual void defend() { } 
+    void pickItem() { } 
+    virtual void castSpell() { } 
 
-    void setExp(int exp){
-        this->exp = exp;
-    }
-
-    void setPoints(int points){
-        this->points = points;
-    }
-
-    void setHealth(int health){
-        this->health = health;
-    }
-
-    void setMana(int mana){
-        this->mana = mana;
-    }
-    void setArmor(int armor){
-        this->armor = armor;
-    }
-
-    int getHealth(){
-        return health;
-    }
-
-    int getMana(){
-        return mana;
-    }
-
-    int getPoints(){
-        return points;
-    }
-
-    void attack(){
-        
-    }
-
-    void defend(){
-        
-    }
-
-    void pickItem(){
-        
-    }
-
-    void castSpell(){
-        
-    }
 };
-   
-#endif
+
+
+#endif //__PLAYER_HPP__
 
