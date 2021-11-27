b5ee5da01f66992c20a53f90361c6fbb287019f4
ellenyim8 <eyim009@ucr.edu>
2021-11-27 12:58:53 -0800

    added mob depending on player role

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <stdlib.h>
+#include <time.h>
 
 // class definitions
 #include "game.hpp"
@@ -13,8 +15,14 @@ using std::cin;
 
 // MAIN DRIVER CODE STARTS HERE
 int main() {
+    srand(time(NULL)); // initialize random seed.
+
+    const string monsters[3] = {"mad king", "witch", "dragon"};
+    int randomMob = rand() % 4; 
+
     Game game;
     Player player;
+
     int choice = 0;
 
     choice = game.start();
@@ -26,6 +34,25 @@ int main() {
     cout << "Wise choice.\n";
  
     player = game.createPlayer();
+
+    Mob mob(monsters[randomMob], monsters[randomMob]);
+
+    if (player.getRole() == "Paladin") {
+        cout << "Your role = Paladin. \nThe mob you will be fighting is: "; 
+        cout << monsters[randomMob] << endl; 
+        
+    }
+    else if (player.getRole() == "Warrior"){ 
+        cout << "Your role = Warrior. \nThe mob you will be fighting is: "; 
+        cout << monsters[randomMob] << endl; 
+        
+    }
+    else if (player.getRole() == "Wizard") {
+        cout << "Your role = Wizard. \nThe mob you will be fighting is: "; 
+        cout << monsters[randomMob] << endl; 
+        
+    }
+    
 
     if (player.getHealth() <= 0) {
 	      game.death(); 
