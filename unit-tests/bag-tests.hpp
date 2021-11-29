7a504f24d97e05d7a9d54937dd6f8cc44dfe4807
dramos2001 <damianramos00@gmail.com>
2021-11-29 15:50:41 -0800

    Added test files for our project's classes, and made most tests for the Player class

---
+++ unit-tests/bag-tests.hpp
@@ -0,0 +1,10 @@
+// Contains tests for the bag class
+// Tests each function of the bag class in different scenarios/cases
+
+#ifndef __BAG_TESTS_HPP__
+#define __BAG_TESTS_HPP__
+
+#include "gtest/gtest.h"
+#include "bag.h"
+
+#endif
