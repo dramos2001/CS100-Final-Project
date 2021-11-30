01b28743f1af7a77086358ae4d11995284cd896d
jasonguan067 <jasonguan067@gmail.com>
2021-11-30 10:30:28 -0800

    little fixes

--- bag.h
+++ bag.h
@@ -15,26 +15,15 @@ public:
     Bag() { }
 
     void displayBag(std::ostream& os) {  // added ostream for unit testing
-        os << "Your bag contains: " << item.size() << " potions" <<  std::endl;
-     /*   for(unsigned int i = 0; i < item.size(); i++){
-            os << item.at(i).getItemName() << " : " << item.at(i).getQuantity() << std::endl;
-        } */ 
+        os << "Your bag contains: " <</* item.at(0).getQuantity()*/ item.size() << " potions "  /* item.at(0).getItemName()*/ <<  std::endl;
+ 
     }
 
     void add(Item it) {
         item.push_back(it);
+	//item.at(0).setQuantity(getQuantity() + 1);
     }
 
-    //void add(string it) {
-    //    Item* i = new Item(it);
-    //    item.push_back(i);
-    //    _size++; 
-    //    items[_size] = &i;    
-    //}
-//    void add(string it) { 
-//	Item i = new Item(it);
-//	items[_size++] = i; 
-    //}
     
     void remove() {item.pop_back();}
     bool empty() {  return (item.size() == 0); }
